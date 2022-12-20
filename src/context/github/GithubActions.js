cd8882626db585ce666b90e8ff33455a4d547026
IdanKfir <zunzer244@gmail.com>
2022-12-20 05:13:53 +0200

    fixAuth

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -4,7 +4,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 const github = axios.create({
   baseURL: GITHUB_URL,
-  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+  // prettier-ignore
+  headers: { "Authorization": `token ${GITHUB_TOKEN}` },
 });
 
 // Get search results
